7caca6daf2c30e4c804297772efd77d1f6d1a71d
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-12-22 20:52:04 -0500

    Update javascript app Level 1

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,63 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Loop through and use d3 to append one table row `tr` for each UFO report object
+tableData.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+// Use 'Object.entries' to console.log each UFO report value
+    Object.entries(ufoReport).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Select the button 
+// Use the `on` function in d3 to attach an event to the handler function
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input date to get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input value
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Filter data with multiple filters
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue||
+                                                    sighting.city === inputValue ||
+                                                    sighting.state === inputValue ||
+                                                    sighting.country === inputValue ||
+                                                    sighting.shape === inputValue);
+   
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+
+    console.log(selections);
+
+    // Append one table row `tr` for each UFO report object
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each UFO report value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
